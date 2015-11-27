8024123026244cd310a40d756921fbba6d4a58db
wanghao1891 <wanghao1891@126.com>
2015-11-27 15:12:12 +0800

    Create a sample, filter the heading.

--- node.js/markdown/app.js
+++ node.js/markdown/app.js
@@ -5,21 +5,47 @@ app.use(express.static('public'));
 
 var port = process.env.PORT || 6008;
 app.listen(port, '0.0.0.0', function() {
-    console.log('Server listening on port', port);
+  console.log('Server listening on port', port);
 });
 
 var marked = require('marked');
-console.log(marked('I am using __markdown__.'));
+/*console.log(marked('I am using __markdown__.'));
 
 marked.setOptions({
-    renderer: new marked.Renderer(),
-    gfm: true,
-    tables: true,
-    breaks: false,
-    pedantic: false,
-    sanitize: true,
-    smartLists: true,
-    smartypants: false
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: true,
+  smartLists: true,
+  smartypants: false
 });
 
 console.log(marked('I am using __markdown__.'));
+
+var renderer = new marked.Renderer();
+
+renderer.heading = function (text, level) {
+  console.log('text:', text, 'level:', level);
+  var escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
+
+  return '<h' + level + '><a name="' +
+    escapedText +
+    '" class="anchor" href="#' +
+    escapedText +
+    '"><span class="header-link"></span></a>' +
+    text + '</h' + level + '>';
+};
+
+console.log(marked('# heading+', { renderer: renderer }));*/
+
+var renderer = new marked.Renderer();
+
+renderer.heading = function(text, level, raw, origin) {
+  console.log('arguments:', arguments);
+  return origin;
+};
+
+console.log('parse', marked('## heading 2\n### heading 3'));
+console.log('filter',marked('## heading 2\n### heading 3', {renderer: renderer}));
